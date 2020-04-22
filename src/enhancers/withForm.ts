3a0502edd5c895e138bed0098711177991f513e2
Josh Gretz <jgretz@truefit.io>
2020-04-22 14:52:14 -0400

    add ability to specify a function to lazy evaluate the formConfig props

--- src/enhancers/withForm.ts
+++ src/enhancers/withForm.ts
@@ -1,17 +1,24 @@
-import {EnhancerContext, EnhancerResult} from '@truefit/bach';
+import {EnhancerContext, EnhancerResult, isFunction, PROPS} from '@truefit/bach';
 import {useForm, UseFormOptions} from 'react-hook-form';
 
-export default <T>(formConfig?: UseFormOptions<T>) => ({
+type FormConfigParameter<T, P> = UseFormOptions<T> | ((props: P) => UseFormOptions<T>) | undefined;
+
+export default <T, P>(formConfig: FormConfigParameter<T, P>) => ({
   generateNewVariable,
 }: EnhancerContext): EnhancerResult => {
   const formConfigAlias = generateNewVariable();
+  const resolveFormConfigAlias = generateNewVariable();
+  const resolveFormConfig = isFunction(formConfig)
+    ? formConfig
+    : (): UseFormOptions<T> => formConfig as UseFormOptions<T>;
 
   return {
     dependencies: {
       useForm,
-      [formConfigAlias]: formConfig,
+      [resolveFormConfigAlias]: resolveFormConfig,
     },
     initialize: `
+      const ${formConfigAlias} = ${resolveFormConfigAlias}(${PROPS});
       const formContext = useForm(${formConfigAlias});
     `,
     props: ['formContext'],
